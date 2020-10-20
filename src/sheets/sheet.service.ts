4030f8c6cfef471eeb4b55ac20351287e2654e60
RemyBosviel <remy.bosviel@gmail.com>
2020-10-20 14:03:50 +0200

    prototype of a method for completing eventStartDate and eventEndDate of sheets

--- src/sheets/sheet.service.ts
+++ src/sheets/sheet.service.ts
@@ -7,11 +7,17 @@ import {SheetCircuitEnum} from '../core/enums/sheetCircuit.enum';
 import {of} from 'rxjs';
 import {SheetStatusEnum} from '../core/enums/sheetStatus.enum';
 import QuerySnapshot = FirebaseFirestore.QuerySnapshot;
+import {EventModel} from '../core/models/event.model';
+import {Request} from 'firebase-functions/lib/providers/https';
+import * as express from 'express';
 
 const transcriptCreationMailTemplateId = 'transcription_creation_email';
 const newSheetRedactionMailTemplateId = 'sheet_creation_email';
 const newMapMailTemplateId = 'map_creation_email';
 const newSheetToValidateTemplateId = 'validation_creation_email';
+const cors = require('cors')({
+    origin: true
+});
 
 const sendSheetNotificationMail = (mailId: string, userIds: string[], sheet:SheetModel) => {
     return admin.firestore().collection('users').where('uid', 'in', userIds).get()
@@ -58,3 +64,50 @@ export const sheetService = {
         return of();
     }
 };
+
+export const addEventStartAndEndDates = (req: Request, res: express.Response): Promise<void> => {
+    return cors(req, res, () => {
+        return admin.firestore().collection('sheets').get()
+            .then((sheetSnapshots: QuerySnapshot) => {
+                sheetSnapshots.forEach((sheetSnapshot: DocumentSnapshot) => {
+                    const sheet = sheetSnapshot.data() as SheetModel;
+                    if (!sheet.eventStartDate) {
+                        admin.firestore().collection('events').where('id', '==', sheet.relatedEventsIds[0]).limit(1).get()
+                            .then((eventSnapshots: QuerySnapshot) => {
+                                if (eventSnapshots.docs.length > 0) {
+                                    const relatedEvent = eventSnapshots.docs[0].data() as EventModel;
+                                    admin.firestore().collection('sheets').doc(sheet.id).update({
+                                        eventStartDate: relatedEvent.startDate,
+                                        eventEndDate: relatedEvent.endDate
+                                    });
+                                }
+                            }).catch((error) => {res.status(503).send({error: JSON.stringify(error)})});
+                    }
+                });
+            }).catch((error) => {res.status(503).send({error: JSON.stringify(error)})});
+    });
+}
+
+export const temp2 = (req: Request, res: express.Response): Promise<void> => {
+    let promises: Promise<any>[] = [];
+    promises.push(admin.firestore().collection('sheets').get()
+        .then((sheetSnapshots: QuerySnapshot) => {
+            sheetSnapshots.forEach((sheetSnapshot: DocumentSnapshot) => {
+                const sheet = sheetSnapshot.data() as SheetModel;
+                if (!sheet.eventStartDate) {
+                    promises.push(admin.firestore().collection('events').where('id', '==', sheet.relatedEventsIds[0]).limit(1).get()
+                        .then((eventSnapshots: QuerySnapshot) => {
+                            if (eventSnapshots.docs.length > 0) {
+                                const relatedEvent = eventSnapshots.docs[0].data() as EventModel;
+                                promises.push(admin.firestore().collection('sheets').doc(sheet.id).update({
+                                    eventStartDate: relatedEvent.startDate,
+                                    eventEndDate: relatedEvent.endDate
+                                }));
+                            }
+                        }).catch((error) => {promises.push(error)}));
+                }
+            });
+        }).catch((error) => {promises.push(error)}));
+    return cors(req, res, () => { Promise.all(promises).then((values) => res.send(values)) });
+}
+
