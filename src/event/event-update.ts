0df0851775a82f58a4af23e73901694e9b335438
RemyBosviel <remy.bosviel@gmail.com>
2020-10-23 11:43:08 +0200

    edited the onEventUpdated method in event-update.ts as requested

--- src/event/event-update.ts
+++ src/event/event-update.ts
@@ -2,23 +2,13 @@ import {Change} from 'firebase-functions';
 import {QueryDocumentSnapshot} from 'firebase-functions/lib/providers/firestore';
 import {sheetService} from '../sheets/sheet.service';
 import {EventModel} from '../core/models/event.model';
+import _ = require('lodash');
 
 export const onEventUpdated = (snapshot: Change<QueryDocumentSnapshot>, context: any) => {
     const oldEvent = snapshot.before.data() as EventModel;
     const newEvent = snapshot.after.data() as EventModel;
-    if (
-        oldEvent.course !== newEvent.course ||
-        oldEvent.chapter !== newEvent.chapter ||
-        oldEvent.university !== newEvent.university ||
-        oldEvent.startDate !== newEvent.startDate ||
-        oldEvent.endDate !== newEvent.endDate ||
-        oldEvent.relatedFiles !== newEvent.relatedFiles ||
-        oldEvent.recorder !== newEvent.recorder ||
-        oldEvent.transcripter !== newEvent.transcripter ||
-        oldEvent.teacher !== newEvent.teacher ||
-        oldEvent.sheetMaker !== newEvent.sheetMaker ||
-        oldEvent.onlineCourse !== newEvent.onlineCourse ||
-        oldEvent.courseLink !== newEvent.courseLink
+    if (_.isEqual(_.omit(oldEvent, ['id', 'createdAt', 'updatedAt', 'createdBy', 'location', 'previousYearSheet', 'status']),
+        _.omit(newEvent, ['id', 'createdAt', 'updatedAt', 'createdBy', 'location', 'previousYearSheet', 'status']))
     ) {
         sheetService.updateSheetsFromEvent(newEvent);
     }
